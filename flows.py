a4fab38c4af80874d8c779dbe5bd0bb906feff80
David Ainslie <dainslie@gmail.com>
2023-07-06 19:53:35 +0100

    Modified flows

---
+++ flows.py
@@ -0,0 +1,15 @@
+import httpx
+from prefect import flow, task
+
+@task
+def fetch_cat_fact():
+    return httpx.get("https://catfact.ninja/fact?max_length=140").json()["fact"]
+
+@task
+def formatting(fact: str):
+    return fact.title()
+
+@flow
+def scooby():
+    fact = fetch_cat_fact()
+    print(formatting(fact))
